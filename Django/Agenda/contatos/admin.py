a5be6c06ecd39d8380529d0c1fa54a4aecca01e5
Gustavo De Araujo <gugaraujo01@gmail.com>
2022-03-10 22:04:20 -0400

    sistema de busca

--- Django/Agenda/contatos/admin.py
+++ Django/Agenda/contatos/admin.py
@@ -3,11 +3,12 @@ from .models import Categoria, Contato
 
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria')
+    list_display = ('id', 'nome', 'sobrenome', 'telefone', 'email', 'categoria', 'mostrar')
     list_display_links = ('id', 'nome', 'sobrenome')
     # list_filter = ('id', 'nome', 'sobrenome')
     list_per_page = 10
     search_fields = ('id', 'nome', 'sobrenome', 'telefone')
+    list_editable = ('telefone', 'mostrar')
 
 
 admin.site.register(Categoria)
